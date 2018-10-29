ece8a356a99875663cdcd93c4f484b642c7c159f
dariuslancranjan <43954603+dariuslancranjan@users.noreply.github.com>
2018-10-29 15:38:42 +0200

    Add files via upload

--- orientation.js
+++ orientation.js
@@ -7,6 +7,6 @@ window.addEventListener("deviceorientation", on_device_orientation);
 function on_device_orientation(e)
 {
 	document.getElementById("id_alpha").innerHTML = e.alpha;
-	document.getElementById("id_beta").innerHTML = e.beta.
+	document.getElementById("id_beta").innerHTML = e.beta;
 	document.getElementById("id_gamma").innerHTML = e.gamma;
 }
