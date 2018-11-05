a2885b1429efd3b65b46dc01682e3008f9c395a0
dariuslancranjan <43954603+dariuslancranjan@users.noreply.github.com>
2018-11-05 13:41:42 +0200

    Add files via upload

--- orientation.js
+++ orientation.js
@@ -32,6 +32,6 @@ function on_device_motion(e)
 	document.getElementById("id_acc_x").innerHTML = Math.round(acc_x*100)/100;
 	document.getElementById("id_acc_y").innerHTML = Math.round(acc_y*100)/100;
 	
-	
-	
+	document.getElementById("id_rot_x").innerHTML = Math.round(Math.atan(acc_x / acc_z) * 100)/100;
+	document.getElementById("id_rot_y").innerHTML = Math.round(Math.atan(acc_y / acc_z) * 100)/100;
 }
