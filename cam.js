4dc31e874a163018c220580a0f78ec120d0b5805
dariuslancranjan <43954603+dariuslancranjan@users.noreply.github.com>
2018-11-12 14:44:16 +0200

    Add files via upload

--- cam.js
+++ cam.js
@@ -18,3 +18,11 @@ function start()
 	var c = {audio:true, video:{facingMode:"environment"}};
 	navigator.mediaDevices.getUserMedia(c).then(on_success).catch(on_failure);
 }
+
+function on_touch(e)
+{
+	var canvas = document.getElementById("id_canvas");
+	var context = canvas.getContext("2d");
+	var vide = document.getElementById("id_video");
+	context.drawImage()
+}
